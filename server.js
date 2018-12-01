0c56267650590d07d6ffb9b8b82c5a4185f0bbb2
Alyssa Liguori <alyssa.liguori@gmail.com>
2018-12-01 11:18:51 -0500

    mongoose addition

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const superagent = require('superagent');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://${process.env.TEST_DB_USERNAME}:${process.env.TEST_DB_PASSWORD}@ds123454.mlab.com:23454/kentaro-test');
+
+const Schema = mongoose.Schema;
 
 app.listen(PORT, () => {
     console.log('The server is now running on port' + PORT);
 });
 
-
+app.get('*',(req, res)=>{
+    res.send('<img src=http://http.cat/500" />');
+});
+function errorHandler(err, res) {
+    console.error(err);
+    if(res) res.status(500).send('Error.');
+}
 
