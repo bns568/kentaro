6a7a398848a3d57b74358d58a06994b12721170f
JasonCDW <dw.jay.12@gmail.com>
2018-12-05 18:33:16 -0500

    fixed all links, need to fix style on a few pages

--- public/template/nav.js
+++ public/template/nav.js
@@ -1,2 +1,7 @@
-let nav = $('#nav-template').html("<aside>kentaro kameyama</aside>")
+let nav = $('#nav-template').html("<p id='nav-main'></p>")
+$('#nav-main').append("<a class='link' href = '../pages/home.html'><p class='shop'>home</p></a>")
+$('#nav-main').append("<a class='link' href = '../pages/shop.html'><p class='shop'>shop women</p></a>")
+$('#nav-main').append("<a class='link' href = '../pages/about.html'><p class='shop'>about</p></a>")
+$('#nav-main').append("<a class='link' href = '../pages/contact.html'><p class='shop'>contact us</p></a>")
+$('#nav-main').append("<p class='shop'>projects</p>")
 let navTemplate = Handlebars.compile(nav)
