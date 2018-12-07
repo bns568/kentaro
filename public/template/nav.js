e2203373fb1274047ac529b8b07afd7eccdbb731
‘Brittany <brittany.stubbs1827@gmail.com>
2018-12-06 19:54:28 -0500

    changed nav

--- public/template/nav.js
+++ public/template/nav.js
@@ -1,7 +1,21 @@
+let binary = function(){
+    let numStr = "";
+    for (let i = 0; i < 6; i++){
+        numStr += Math.floor(Math.random() * 2);
+    }
+    return numStr
+}
+
+let home = binary();
+let collection = binary();
+let about = binary();
+let contact = binary();
+let projects = binary();
+
 let nav = $('#nav-template').html("<p id='nav-main'></p>")
-$('#nav-main').append("<a class='link' href = '../pages/home.html'><p class='shop'>home</p></a>")
-$('#nav-main').append("<a class='link' href = '../pages/shop.html'><p class='shop'>shop women</p></a>")
-$('#nav-main').append("<a class='link' href = '../pages/about.html'><p class='shop'>about</p></a>")
-$('#nav-main').append("<a class='link' href = '../pages/contact.html'><p class='shop'>contact us</p></a>")
-$('#nav-main').append("<a class='link' href = '../pages/projects.html'><p class='shop'>projects</p></a>")
+$('#nav-main').append(`<a class='link' href = '../pages/home.html'><p class='shop'>${home}</p></a>`)
+$('#nav-main').append(`<a class='link' href = '../pages/shop.html'><p class='shop'>${collection}</p></a>`)
+$('#nav-main').append(`<a class='link' href = '../pages/about.html'><p class='shop'>${about}</p></a>`)
+$('#nav-main').append(`<a class='link' href = '../pages/contact.html'><p class='shop'>${contact}</p></a>`)
+$('#nav-main').append(`<a class='link' href = '../pages/projects.html'><p class='shop'>${projects}</p></a>`)
 let navTemplate = Handlebars.compile(nav)
