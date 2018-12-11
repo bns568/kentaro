f928dd406a0cc2d061bb009f75ace524240df5e8
‘Brittany <brittany.stubbs1827@gmail.com>
2018-12-11 10:06:01 -0500

    changed order of nav

--- public/template/nav.js
+++ public/template/nav.js
@@ -14,8 +14,8 @@ let projects = binary();
 
 let nav = $('#nav-template').html("<p id='nav-main'></p>")
 $('#nav-main').append(`<a class='link' href = '../pages/home.html'><p class='shop'>${home}</p></a>`)
-$('#nav-main').append(`<a class='link' href = '../pages/shop.html'><p class='shop'>${collection}</p></a>`)
 $('#nav-main').append(`<a class='link' href = '../pages/about.html'><p class='shop'>${about}</p></a>`)
-$('#nav-main').append(`<a class='link' href = '../pages/contact.html'><p class='shop'>${contact}</p></a>`)
+$('#nav-main').append(`<a class='link' href = '../pages/shop.html'><p class='shop'>${collection}</p></a>`)
 $('#nav-main').append(`<a class='link' href = '../pages/projects.html'><p class='shop'>${projects}</p></a>`)
+$('#nav-main').append(`<a class='link' href = '../pages/contact.html'><p class='shop'>${contact}</p></a>`)
 let navTemplate = Handlebars.compile(nav)
